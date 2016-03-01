7cdf0d5c139a135cb5bf088b316169331aeac700
Thomas De Groote <appledeveloper@degroote.org>
2016-03-01 13:54:04 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,11 +2,15 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape("https://www.indiegogo.com/projects/lumina-2/#/");
+$matches = array();
+if(preg_match('/gon\.campaign\=\{(.+?)\}\;/', $html, $matches)){
+  print_r($matches);
+}
 //
 // // Find something on the page using css selectors
 // $dom = new simple_html_dom();
